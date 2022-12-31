3345ca03057f59e34a77ddabc447d6a643e03872
Sofiane Larbi <sofiane@Sofianes-M1-MacBook-Pro.local>
2022-12-31 16:32:11 -0500

    init

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/ipfs/go-log/v2"
+	"github.com/libp2p/go-libp2p"
+	dht "github.com/libp2p/go-libp2p-kad-dht"
+	"github.com/libp2p/go-libp2p/core/peer"
+	drouting "github.com/libp2p/go-libp2p/p2p/discovery/routing"
+	dutil "github.com/libp2p/go-libp2p/p2p/discovery/util"
+	"github.com/multiformats/go-multiaddr"
+	"sync"
+)
+
+var logger = log.Logger("rendezvous")
+
+func findPeers() {
+
+}
+
+func main() {
+	log.SetAllLoggers(log.LevelWarn)
+	log.SetLogLevel("rendezvous", "info")
+	logger.Info("Hello World, starting node...")
+	config, err := ParseFlags()
+	if err != nil {
+		panic(err)
+	}
+	host, err := libp2p.New(libp2p.ListenAddrs([]multiaddr.Multiaddr(config.ListenAddresses)...))
+	if err != nil {
+		panic(err)
+	} else {
+		logger.Info("Host created. We are:", host.ID())
+		logger.Info(host.Addrs())
+	}
+	ctx := context.Background()
+	kademliaDHT, err := dht.New(ctx, host)
+	if err != nil {
+		panic(err)
+	}
+	logger.Debug("Bootstrapping the DHT")
+	if err = kademliaDHT.Bootstrap(ctx); err != nil {
+		panic(err)
+	}
+	var wg sync.WaitGroup
+	for _, peerAddy := range config.BootstrapPeers {
+		peerInfo, _ := peer.AddrInfoFromP2pAddr(peerAddy)
+		if peerInfo != nil {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				if err := host.Connect(ctx, *peerInfo); err != nil {
+					logger.Warning(err)
+				} else {
+					logger.Info("Connection established with bootstrap node:", *peerInfo)
+				}
+			}()
+		}
+	}
+	wg.Wait()
+
+	logger.Info("Announcing ourselves...")
+	routingDiscovery := drouting.NewRoutingDiscovery(kademliaDHT)
+	dutil.Advertise(ctx, routingDiscovery, config.RendezvousString)
+	logger.Debug("Successfully announced!")
+
+	logger.Info("Searching for other peers...")
+	peers, err := routingDiscovery.FindPeers(ctx, config.RendezvousString)
+	if err != nil {
+		panic(err)
+	}
+	for peer := range peers {
+		if peer.ID == host.ID() {
+			continue
+		}
+		logger.Info("Found peer:", peer)
+		peerIp := peer.Addrs[0].String()
+		fmt.Println(peerIp)
+	}
+
+}
