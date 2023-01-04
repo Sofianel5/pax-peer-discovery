42ef52662f45501cf329ea9b9b29b65aa4c82aa3
Sofiane Larbi <sofianef1110@gmail.com>
2023-01-03 21:23:45 -0500

    update

--- protocol.go
+++ protocol.go
@@ -1,18 +1,56 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"net"
+	"strings"
 )
 
-func checkPeerHello(ip string) bool {
-	// send TCP hello to peer
+const MAX_PEERS = 10
 
-	var conn net.Conn
-	conn, err := net.Dial("tcp", ip)
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			return
+		}
+		if string(buf[:n]) == "send_peers" {
+			// Send peers
+		}
+	}
+}
+
+func runServer() {
+	l, err := net.Listen("tcp", ":696969")
+	logger.Info("Listening on port 696969")
 	if err != nil {
-		logger.Error("Error connecting to peer:", err)
-		return false
+		panic(err)
+	}
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			panic(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func getPeers(peer string) []string {
+	// Retrieve and parse peers list from peer
+	conn, err := net.Dial("tcp", peer)
+	if err != nil {
+		panic(err)
 	}
 	defer conn.Close()
-	return true
+	conn.Write([]byte("send_peers"))
+	var buf bytes.Buffer
+	_, err := io.Copy(&buf, conn)
+	if err != nil {
+		panic(err)
+	}
+	return strings.Split(buf.String(), ",")
 }
