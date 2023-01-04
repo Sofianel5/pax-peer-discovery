e81e7f84f23cbc533b753936df33e609546e9c24
Sofiane Larbi <sofianef1110@gmail.com>
2023-01-03 22:51:32 -0500

    update

--- setup.sh
+++ setup.sh
@@ -8,5 +8,3 @@ wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
 rm go1.19.4.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
-go get
