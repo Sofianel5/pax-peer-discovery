5a123fc0fd0f22b32cbe941a3a54686c746452cb
Sofiane Larbi <sofianef1110@gmail.com>
2023-01-03 22:40:13 -0500

    update

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Check for root
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+apt upgrade -y
+wget https://go.dev/dl/go1.19.4.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.19.4.linux-amd64.tar.gz
+rm go1.19.4.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
