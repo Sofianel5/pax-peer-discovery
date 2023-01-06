87cb0b7f13590c6ce59294b102cfc87d002c5ac9
Sofiane Larbi <sofianef1110@gmail.com>
2023-01-05 23:18:15 -0500

    update

--- mpc.go
+++ mpc.go
@@ -1,5 +1,32 @@
 package main
 
-func runmpc() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
 
+const MPC_DIR = "./workspace/mp-spdz"
+
+func run2pc(app, input, counterparty string, player int) (err error) {
+	// Write input to file in Player-Data/Input-P<party>-0
+	f, err := os.OpenFile(fmt.Sprintf("%s/Player-Data/Input-P%d-0", MPC_DIR, player), os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	_, err = f.WriteString(input)
+	if err != nil {
+		return
+	}
+	// Run MPC
+	cmd := exec.Command(fmt.Sprintf("//mascot-party.x -N 2 -h %s %d %s", counterparty, player, app))
+	cmd.Dir = MPC_DIR
+	out, err := cmd.CombinedOutput()
+	logger.Info("MPC Protocol Complete")
+	logger.Info(string(out))
+	if err != nil {
+		logger.Error(err)
+	}
+	return nil
 }
